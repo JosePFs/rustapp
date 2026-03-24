837df5f747d242ea5f95ab4c8aeb5e379cbab987
José Puente Fuentes <jpuentefs@gmail.com>
2026-03-24 21:58:55 +0100

    Remove old database migrations

--- supabase/migrations_archive/013_remove_workout_session_name.sql
+++
@@ -1,3 +0,0 @@
--- Session name removed: the "name" of a session is the workout name (edited by specialist in workout library).
--- Patients cannot rename sessions.
-ALTER TABLE workout_sessions DROP COLUMN IF EXISTS name;
